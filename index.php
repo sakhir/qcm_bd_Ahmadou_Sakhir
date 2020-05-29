25ad2d4b77d28ed520ef5f10b1d3af385f8dcdc5
Your Name <you@example.com>
2020-05-29 02:47:10 +0100

    integration us1

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page d'accueil</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="css/connexion.css">
+   <link rel="stylesheet" type="text/css" href="css/util.css">
+	<!-- <link rel="stylesheet" type="text/css" href="css/main.css"> -->
+</head>
+<body>
+       <div id="cont">
+			<div class="container ">
+			  <div class="row ">
+			    <div class="col-4 ">
+			      <div class="gauche"> <span class="ml-2">  le plaisir de jouer </span></div>
+                   
+			    </div>
+			    <div class="col-8 ">
+			      	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178" method="post">
+					<span class="login100-form-title">
+						Se  Connecter
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
+						<input class="input100" type="text" name="username" placeholder="Login">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Please enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+					</div>
+
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Se connecter
+						</button>
+						<span class="txt1 p-t-5">
+							Vous n'avez pas de compte ?
+						</span><br>
+
+						<a href="src/inscription.php" class="txt3">
+							S'inscrire pour jouer
+						</a>
+					</div>
+
+					
+				</form>
+			</div>
+		</div>
+		<div class="arc"> </div>
+		<img id="logosa" src="images/logo-QuizzSA.png">
+	</div>
+	
+			    </div>
+			  </div>
+			  
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ src/inscription.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page d'inscription </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="../css/inscription.css">
+   <link rel="stylesheet" type="text/css" href="../css/util.css">
+<!-- 	<link rel="stylesheet" type="text/css" href="../css/main.css"> -->
+</head>
+<body>
+       <div id="cont">
+			<div class="container ">
+			  <div class="row ">
+			    <div class="col-4 ">
+			      <div class="gauche"> <span class="ml-2">  le plaisir de jouer </span></div>
+                   
+			    </div>
+			    <div class="col-8 ">
+			      	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form  p-l-30 p-r-55 p-t-50" method="post">
+					<span class="login100-form-title">
+						S'inscrire pour tester votre niveau en culture generale
+					</span>
+					<div class="row">
+					   <div class="col-6 "> 
+		                    <label class="txt3">Prenom</label>
+							<div class="wrap-input100  m-b-16" >
+
+								<input class="input100" type="text" name="prenom" placeholder="prenom">
+								<span class="focus-input100"></span>
+							</div>
+                      </div>
+                      <div class="col-6 "> 
+		                    <label class="txt3">Nom</label>
+							<div class="wrap-input100  m-b-16" >
+
+								<input class="input100" type="text" name="nom" placeholder="nom">
+								<span class="focus-input100"></span>
+							</div>
+                      </div>
+                     </div>
+                    
+                     <div class="row">
+
+                     <div class="col-6 "> 
+		                    <label class="txt3">Login</label>
+							<div class="wrap-input100  m-b-16" >
+
+								<input class="input100" type="text" name="login" placeholder="login">
+								<span class="focus-input100"></span>
+							</div>
+                      </div>
+                    <div class="col-6 "> 
+		                    <label class="txt3">Mot de pass</label>
+							<div class="wrap-input100  m-b-16" >
+
+							    <input class="input100" type="password" name="pwd" placeholder="Password">
+								<span class="focus-input100"></span>
+							</div>
+                      </div>  
+
+                     </div>
+
+
+                     <div class="row">
+                    <div class="col-6 "> 
+		                    <label class="txt3">Confirmer mot de pass</label>
+							<div class="wrap-input100  m-b-16" >
+
+							    <input class="input100" type="password" name="cpwd" placeholder="Password">
+								<span class="focus-input100"></span>
+							</div>
+                      </div> 
+                    <div class="col-6 "> 
+		                    <label class="txt3">avatar</label>
+							<div class="wrap-input100  m-b-16" >
+
+								<input class="inputf" type="file" name="avatar" >
+								<span class="focus-input100"></span>
+							</div>
+                      </div> 
+
+                     </div>
+
+                     <div class="row">
+
+                     <div class="col-3 "> 
+		            
+							<div class=" " >
+
+								<hr style="width: 150%;height: 2px;background-color: black;border-radius: 50%;margin-top: 70%;">
+								
+							</div>
+                      </div>
+                    <div class="col-6 "> 
+		                 
+							<div class="wrap-input100  m-b-16" >
+
+							   <img id="av" src="../images/av.png">
+								
+							</div>
+                      </div>  
+                    <div class="col-3 "> 
+		 
+							<div class="" >
+
+							<hr style="width: 150%;height: 2px;background-color: black;border-radius: 50%;margin-top: 70%;margin-right: 30%;">
+							
+							</div>
+                      </div>
+
+                     </div>
+
+
+					
+				
+
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							S'inscrire
+						</button>
+
+						<a href="../index.php" class="txt3">
+							Se connecter
+						</a>
+					</div>
+
+					
+				</form>
+			</div>
+		</div>
+		
+	</div>
+	<div class="arc"> </div>
+	<img id="logosa" src="../images/logo-QuizzSA.png">
+			    </div>
+			  </div>
+			  
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+</body>
+</html>
